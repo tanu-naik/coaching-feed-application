d8dbf449f7251eed009d9f9edfcf284107eb6e82
sanjugowdam <sanjaym3236@gmail.com>
2026-05-19 01:59:50 +0530

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Optional but good practice
+  /* standard options go here (like images, rewrites, redirects) */
 };
+
+export default nextConfig;
